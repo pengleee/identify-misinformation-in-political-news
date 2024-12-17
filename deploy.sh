53e3e65a36bc7294ab402407a299be98abf686f2
Peng Li <pengleee@outlook.com>
2024-12-17 17:44:52 -0500

    Reorganize the directory

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# PUSH WEBSITE TO GU DOMAINS 
-printf 'Would you like to push to GU domains? (y/n)? '
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then 
-    # rsync -alvr --delete _site/* jfhgeorg@gtown.reclaimhosting.com:/home/jfhgeorg/public_html/dsan-5000/
-    rsync -alvr --delete _site/* penglige@pengli.georgetown.domains:~/public_html/proj5000/
-else
-    echo NOT PUSHING TO GU DOMAINS!
-fi
